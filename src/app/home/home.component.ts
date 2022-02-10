8dfc94eae4bb16e204b0edfa86b80b7a92c5db3e
Stanley Tang <stanleytang0726@gmail.com>
2022-02-09 21:54:44 -0500

    changed initialization structure

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as THREE from 'three';
 
 @Component({
@@ -10,7 +11,11 @@ export class HomeComponent implements OnInit {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
   renderer = new THREE.WebGLRenderer();
-  
+  geometries:any = [];
+  textures:any = [];
+  materials:any = [];
+  meshes:any = [];
+
 
   constructor() { }
 
@@ -20,23 +25,51 @@ export class HomeComponent implements OnInit {
     let scene = this.scene;
     let camera = this.camera;
     document.body.appendChild(this.renderer.domElement);
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    this.scene.add( cube );
 
-    this.camera.position.z = 5;
+    let pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(5,5,5)
+
+    this.scene.add(pointLight);
+
+    this.camera.position.z = 30;
+
+    this.initGeo();
+    this.initTextures();
+    for( let key of Object.keys(this.textures)) {
+      this.initBasicMat(key);
+    }
+    for(let key of Object.keys(this.geometries)){
+      this.initMesh(key, key, 'MUG' )
+    }
+    
+
+    const controls = new OrbitControls(camera,renderer.domElement);
 
     var animate = function () {
       requestAnimationFrame( animate );
-
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
+      controls.update();
       renderer.render( scene, camera );
     };
 
     animate();
   }
 
+  initGeo(): void {
+    this.geometries['planet-ring'] = new THREE.RingGeometry( 15, 20,100 );
+    this.geometries['planet'] = new THREE.SphereGeometry( 10 );
+  }
+
+  initTextures(): void {
+    this.textures['MUG'] = new THREE.TextureLoader().load('./assets/Mug_root_beer_logo.png');
+  }
+
+  initBasicMat(texture : string):void {
+    this.materials[texture] =  new THREE.MeshBasicMaterial( { map:this.textures[texture]} );
+  }
+
+  initMesh(name:string, geometry:string, material:string){
+    this.meshes[name] = new THREE.Mesh( this.geometries[geometry], this.materials[material]);
+    this.scene.add(this.meshes[name])
+  }
 }
+
